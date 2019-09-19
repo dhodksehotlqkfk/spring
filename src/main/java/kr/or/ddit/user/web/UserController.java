41856d8b47a777d7699faa56b9b718b5bd5f35c5
dhodksehotlqkfk <dhodksehotlqkfk.chawnghks@gmail.com>
2019-09-19 13:33:38 +0900

    spring mvc  . user 상세조회 화면 이관  . 사용자 이미지 요청 처리  . 사용자 등록  . 사용자 수정  . 파일 업로드 테스트 코드 작성

--- src/main/java/kr/or/ddit/user/web/UserController.java
+++ src/main/java/kr/or/ddit/user/web/UserController.java
@@ -1,18 +1,30 @@
 package kr.or.ddit.user.web;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Map;
 
 import javax.annotation.Resource;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
 
 import kr.or.ddit.common.model.Page;
+import kr.or.ddit.user.model.User;
+import kr.or.ddit.user.model.UserValidator;
 import kr.or.ddit.user.service.IUserService;
+import kr.or.ddit.util.FileUtil;
+import kr.or.ddit.util.model.FileInfo;
 
 @RequestMapping("user/")
 @Controller
@@ -92,5 +104,142 @@ public class UserController {
 		return "user/userPagingList";
 	}
 	
+	@RequestMapping(path = "user", method = RequestMethod.GET)
+	public String userDetailView(Model model, String userId) {
+		model.addAttribute("user", userService.getUser(userId));
+		return "user/user";
+	}
+	
+	@RequestMapping(path = "userPicture", method = RequestMethod.GET)
+	public void userPicture(String userId, HttpServletResponse response) throws IOException {
+		User user = userService.getUser(userId);
+		
+		ServletOutputStream sos = response.getOutputStream();
+		
+		File picture = new File(user.getRealfilename());
+		FileInputStream fis = new FileInputStream(picture);
+		
+		byte[] buff = new byte[512];
+		int len = 0;
+		
+		while( (len = fis.read(buff, 0, 512)) != -1 ) {
+			sos.write(buff, 0, len);
+		}
+		
+		fis.close();
+	}
+	
+	/**
+	* Method : userFormView
+	* 작성자 : PC-02
+	* 변경이력 :
+	* @return
+	* Method 설명 : 사용자 등록 화면 요청
+	*/
+	@RequestMapping(path = "userForm", method = RequestMethod.GET)
+	public String userFormView() {
+		return "user/userForm";
+	}
+	
+	/**
+	* Method : userForm
+	* 작성자 : PC-02
+	* 변경이력 :
+	* @param user
+	* @param result
+	* @param picture
+	* @return
+	* Method 설명 : 사용자 등록 요청
+	*/
+	@RequestMapping(path = "userForm", method = RequestMethod.POST)
+	public String userForm(User user, BindingResult result, @RequestPart("picture") MultipartFile picture) {
+		
+		new UserValidator().validate(user, result);
+		
+		if(result.hasErrors()) {
+			return "user/userForm";
+		}else {
+			FileInfo fileInfo = FileUtil.getFileInfo(picture.getOriginalFilename());
+			
+			if(picture.getSize() > 0) {
+				try {
+					picture.transferTo(fileInfo.getFile());
+					user.setFilename(fileInfo.getOrginalFileName());	//파일이름
+					user.setRealfilename(fileInfo.getFile().getPath());	//파일경로
+					
+				} catch (IllegalStateException | IOException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			int insertCnt = userService.insertUser(user);
+			
+			if(insertCnt == 1) {
+				//redirect
+				return "redirect:/user/user?userId=" + user.getUserId();
+			}else {
+				//forward
+				return "user/userForm";
+			}
+		}
+	}
+	
+	/**
+	* Method : userModifyView
+	* 작성자 : PC-02
+	* 변경이력 :
+	* @param model
+	* @param userId
+	* @return
+	* Method 설명 : 사용자 수정 화면 요청
+	*/
+	@RequestMapping(path = "userModify", method = RequestMethod.GET)
+	public String userModifyView(Model model, String userId) {
+		model.addAttribute("user", userService.getUser(userId));
+		return "user/userModify";
+	}
+	
+	/**
+	* Method : userModify
+	* 작성자 : PC-02
+	* 변경이력 :
+	* @param user
+	* @param result
+	* @param picture
+	* @return
+	* Method 설명 : 사용자 수정 요청
+	*/
+	@RequestMapping(path = "userModify", method = RequestMethod.POST)
+	public String userModify(User user, BindingResult result, @RequestPart("picture") MultipartFile picture) {
+		
+		new UserValidator().validate(user, result);
+		
+		if(result.hasErrors()) {
+			return "user/userForm";
+		}else {
+			FileInfo fileInfo = FileUtil.getFileInfo(picture.getOriginalFilename());
+			
+			if(picture.getSize() > 0) {
+				try {
+					picture.transferTo(fileInfo.getFile());
+					user.setFilename(fileInfo.getOrginalFileName());	//파일이름
+					user.setRealfilename(fileInfo.getFile().getPath());	//파일경로
+					
+				} catch (IllegalStateException | IOException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			int insertCnt = userService.updateUser(user);
+			
+			if(insertCnt == 1) {
+				//redirect
+				return "redirect:/user/user?userId=" + user.getUserId();
+			}else {
+				//forward
+				return "user/userForm";
+			}
+		}
+	}
 	
 }
--- src/test/java/kr/or/ddit/user/web/UserControllerTest.java
+++ src/test/java/kr/or/ddit/user/web/UserControllerTest.java
@@ -1,11 +1,19 @@
 package kr.or.ddit.user.web;
 
+import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.fileUpload;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import java.io.File;
+import java.io.FileInputStream;
+
 import org.junit.Test;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.web.servlet.ModelAndView;
 
 import kr.or.ddit.common.model.Page;
 import kr.or.ddit.config.test.WebTestConfig;
@@ -70,5 +78,69 @@ public class UserControllerTest extends WebTestConfig{
 
 		/***Then***/
 	}
+	
+	/**
+	* Method : userDetailViewTest
+	* 작성자 : PC-02
+	* 변경이력 :
+	* @throws Exception
+	* Method 설명 : 사용자 상세정보 조회 테스트
+	*/
+	@Test
+	public void userDetailViewTest() throws Exception {
+		/***Given***/
 
+		/***When***/
+		mockMvc.perform(get("/user/user").param("userId", "brown"))
+			.andExpect(status().isOk())
+			.andExpect(view().name("user/user"))
+			.andExpect(model().attributeExists("user"));
+
+		/***Then***/
+	}
+
+	/**
+	* Method : userFormViewTest
+	* 작성자 : PC-02
+	* 변경이력 :
+	* Method 설명 : 사용자 등록 화면 요청 테스트
+	 * @throws Exception 
+	*/
+	@Test
+	public void userFormViewTest() throws Exception {
+		/***Given***/
+
+		/***When***/
+		MvcResult mvcResult = mockMvc.perform(get("/user/userForm")).andReturn();
+		ModelAndView mav = mvcResult.getModelAndView();
+
+		/***Then***/
+		assertEquals("user/userForm", mav.getViewName());
+	}
+	
+	//사용자 등록 요청 테스트
+	@Test
+	public void userFormTest() throws Exception {
+		File f = new File("src/test/resources/kr/or/ddit/test/sally.png");
+		FileInputStream fis = new FileInputStream(f);
+		
+		MockMultipartFile file = new MockMultipartFile("picture", "sally.png", "", fis);
+		
+		mockMvc.perform(fileUpload("/user/userForm")
+						.file(file)
+						.param("userId", "borwnTest")
+						.param("userNm", "브라운테스트")
+						.param("alias", "곰테스트")
+						.param("reg_dt", "2019-08-08")
+						.param("addr1", "대전광역시 중구 중앙로 76")
+						.param("addr2", "영민빌딩 2층 DDIT")
+						.param("zipcode", "34940")
+						.param("pass", "brownTest1234"))
+			.andExpect(status().is(302));
+//			.andExpect(view().name("redirect:user/user"));
+	}
+	
+	//사용자 수정화면 요청 테스트
+	
+	//사용자 수정 요청 테스트
 }
